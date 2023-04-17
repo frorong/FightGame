c7955f4d18a74daf8e8af572504829b3ad1b731b
Lee-Seungje <moondgod@gmail.com>
2023-04-18 00:18:35 +0900

    Add Boxer

--- src/app/game/style.ts
+++ src/app/game/style.ts
@@ -1,1 +1,72 @@
 import styled from '@emotion/styled';
+
+export const Cage = styled.div`
+  width: 800px;
+  height: 700px;
+  display: flex;
+  background-color: silver;
+  padding-top: 100px;
+  justify-content: space-around;
+`;
+
+export const Boxer = styled.div`
+  width: 100px;
+`;
+
+export const BoxerHead = styled.div`
+  width: 100px;
+  height: 100px;
+  border-radius: 50px;
+  background-color: gray;
+  border: solid 10px black;
+`;
+
+export const BoxerBody = styled.div`
+  width: 100px;
+  height: 180px;
+  border-radius: 20px;
+  background-color: gray;
+  border: solid 10px black;
+`;
+
+export const BoxerWeapon = styled.div`
+  position: relative;
+  top: 240px;
+  width: 250px;
+  height: 30px;
+  background-color: gray;
+  border-radius: 20px;
+  border: solid 10px black;
+`;
+
+export const Punch = styled.div`
+  width: 250px;
+  height: 30px;
+  background-color: gray;
+  border-radius: 20px;
+  transition: ease-in-out 0.5s;
+  border: solid 10px black;
+`;
+
+export const BoxerReg = styled.div`
+  width: 40px;
+  height: 200px;
+  background-color: gray;
+  border-radius: 20px;
+  border: solid 10px black;
+`;
+
+export const FlexBox = styled.div`
+  display: flex;
+  width: 100px;
+  justify-content: space-between;
+`;
+
+export const Kick = styled.div`
+  width: 40px;
+  height: 200px;
+  background-color: gray;
+  border-radius: 20px;
+  transition: ease-in-out 0.5s;
+  border: solid 10px black;
+`;
