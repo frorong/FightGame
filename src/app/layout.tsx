95693b4773bfa5d20c4779c4f45d39f6d951d936
Lee-Seungje <moondgod@gmail.com>
2023-04-19 20:11:01 +0900

    Add title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 'use client';
 import './globals.css';
+import Head from 'next/head';
 
 export default function RootLayout({
   children,
@@ -8,6 +9,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
+      <title>GFC</title>
+      <Head>
+        <meta property="og:title" content="Every GSM" key="title" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>{children}</body>
     </html>
   );
